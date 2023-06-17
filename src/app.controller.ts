b6147322a01dff4c17a0a2311ed026234ef445bb
sakthiveltsmr <sakthiveltsmr@gmail.com>
2023-06-17 14:25:17 +0530

    .

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get(`:id`)
+  findOne(@Param('id') id: string) {
+    return 'retun the single user' + id;
+  }
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
+  @Put(':id')
+  update(@Param('id') id: string, @Body() Body: any) {
+    return 'your data is updated';
   }
 }
