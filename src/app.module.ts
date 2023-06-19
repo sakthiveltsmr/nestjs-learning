626023c5e4bb53606febfe8a726d2e135ded3edd
sakthiveltsmr <sakthiveltsmr@gmail.com>
2023-06-19 21:42:20 +0530

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CartModule } from './cart/cart.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CartModule, UserModule],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
