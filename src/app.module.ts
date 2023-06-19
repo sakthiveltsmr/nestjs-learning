f9ef96fd17b38b0fe74087b3c629e471924d3888
sakthiveltsmr <sakthiveltsmr@gmail.com>
2023-06-19 21:34:58 +0530

    -

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CartModule } from './cart/cart.module';
-import { CartcontrollerController } from './cartcontroller/cartcontroller.controller';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [CartModule, UserModule],
-  controllers: [AppController, CartcontrollerController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/catcontroller/catcontroller.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('catcontroller')
-export class CatcontrollerController {}
