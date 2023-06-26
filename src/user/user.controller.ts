0d043309435e0723fae91e8155bb4b5adb4b5a37
sakthiveltsmr <sakthiveltsmr@gmail.com>
2023-06-26 20:40:39 +0530

    .

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,15 +13,18 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  create(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+    const result = this.userService.create(createUserDto);
+    if (result) {
+      res.status(201).send('user created successfully');
+    }
   }
 
   @Get()
